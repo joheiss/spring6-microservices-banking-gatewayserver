04b105adda17f5b07421d40748df74064b02bb72
Josef Heiss <josef.heiss@jovisco.de>
2024-06-17 21:20:56 +0200

    custom routes

--- src/main/java/com/jovisco/services/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/jovisco/services/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.jovisco.services.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,31 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder) {
+
+		return routeLocatorBuilder.routes()
+			.route(p -> p
+				.path("/api/v1/banking/accounts/**")
+				.filters(f ->f
+					.rewritePath(
+						"/api/v1/banking/accounts/(?<segment>.*)", 
+						"/api/v1/banking/accounts/${segment}"))
+				.uri("lb.ACCOUNTS"))		
+			.route(p -> p
+				.path("/api/v1/banking/loans/**")
+				.filters(f ->f
+					.rewritePath(
+						"/api/v1/banking/loans/(?<segment>.*)", 
+						"/api/v1/banking/loans/${segment}"))
+				.uri("lb.LOANS"))		
+			.route(p -> p
+				.path("/api/v1/banking/cards/**")
+				.filters(f ->f
+					.rewritePath(
+						"/api/v1/banking/cards/(?<segment>.*)", 
+						"/api/v1/banking/cards/${segment}"))
+				.uri("lb.CARDS"))		
+			.build();
+	}
 }
