0b8b012b36cdefadbb74611a24a720dfd1f7f473
Josef Heiss <josef.heiss@jovisco.de>
2024-06-20 16:32:19 +0200

    Http timeouts

--- src/main/java/com/jovisco/services/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/jovisco/services/gatewayserver/GatewayserverApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.gateway.support.RouteMetadataUtils;
 import org.springframework.context.annotation.Bean;
 
 import static org.springframework.cloud.gateway.support.RouteMetadataUtils.CONNECT_TIMEOUT_ATTR;
